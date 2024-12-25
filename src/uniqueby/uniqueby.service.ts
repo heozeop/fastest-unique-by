ffd9bd07020efc600f3553b0806d698e994873e4
crispy <dltmdcks702@cau.ac.kr>
2024-12-25 19:14:35 +0900

    feat: add unique by immutable set

--- src/uniqueby/uniqueby.service.ts
+++ src/uniqueby/uniqueby.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import uniqBy from 'lodash/unionBy';
+import { Set as ImmutableSet } from 'immutable';
 
 @Injectable()
 export class UniquebyService {
@@ -18,5 +19,18 @@ export class UniquebyService {
 
   uniqueByLodash<T, K extends keyof T>(array: T[], key: K) {
     return uniqBy(array, key);
+  }
+
+  uniqueByImmutableSet<T, K extends keyof T>(array: T[], key: K) {
+    const seen = ImmutableSet();
+    return array.filter((item) => {
+      const keyValue = item[key];
+      if (seen.has(keyValue)) {
+        return false;
+      }
+
+      seen.add(keyValue);
+      return true;
+    });
   }
 }
