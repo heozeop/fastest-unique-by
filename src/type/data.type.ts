d9aa8d7ecc3454195fb8644479229864fd78ea83
crispy <dltmdcks702@cau.ac.kr>
2024-12-25 19:22:20 +0900

    feat: init unique by endpoints

---
+++ src/type/data.type.ts
@@ -0,0 +1,13 @@
+export interface Data {
+  key: string;
+  tempValue1: string;
+  tempValue2: string;
+  tempValue3: string;
+  tempValue4: string;
+  tempValue5: string;
+  tempValue6: string;
+  tempValue7: string;
+  tempValue8: string;
+  tempValue9: string;
+  tempValue10: string;
+}
--- src/uniqueby/uniqueby.controller.ts
+++ src/uniqueby/uniqueby.controller.ts
@@ -1,7 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { UniquebyService } from './uniqueby.service';
+import data from '../../data.json';
+import { Data } from 'src/type/data.type';
 
 @Controller('uniqueby')
 export class UniquebyController {
   constructor(private readonly uniquebyService: UniquebyService) {}
+
+  @Get('set')
+  uniqueBySet() {
+    return this.uniquebyService.uniqueBySet(data as Data[], 'key');
+  }
+
+  @Get('lodash')
+  uniqueByLodash() {
+    return this.uniquebyService.uniqueByLodash(data as Data[], 'key');
+  }
+
+  @Get('immutable')
+  uniqueByImmutable() {
+    return this.uniquebyService.uniqueByImmutableSet(data as Data[], 'key');
+  }
+
+  @Get('map')
+  uniqueByMap() {
+    return this.uniquebyService.uniqueByMap(data as Data[], 'key');
+  }
 }
