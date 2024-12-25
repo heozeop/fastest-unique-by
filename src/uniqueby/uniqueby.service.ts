8e9d7cfd06590ce1dc9be51106f07c095401c5fd
crispy <dltmdcks702@cau.ac.kr>
2024-12-25 19:10:28 +0900

    feat: add unique by set

--- src/uniqueby/uniqueby.service.ts
+++ src/uniqueby/uniqueby.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UniquebyService {}
+export class UniquebyService {
+  uniqueBySet<T, K extends keyof T>(array: T[], key: K) {
+    const seen = new Set();
+    return array.filter((item) => {
+      const keyValue = item[key];
+      if (seen.has(keyValue)) {
+        return false;
+      }
+
+      seen.add(keyValue);
+      return true;
+    });
+  }
+}
